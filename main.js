cde58557cde65c2fad0ecba8f88c208a8904b615
kundup <kundup61@hotmail.com>
2025-05-03 06:55:37 -0700

    new game function

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 const canvasel = document.getElementById("canvas");
-const canvasContext = canvasel.getContext("2d");
+const context = canvasel.getContext ("2d");
 
-canvasel.width = 384;
-canvasel.height = 224;
+canvasel.width = 400;
+canvasel.height = 400;
+
+class Enemy {
+
+    constructor(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw (){
+        context. fillStyle = "blue";
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Flying extends Enemy {
+    constructor(x, y, height, width, name){
+        super(x, y, width, height);
+        this.name = name;           
+    }
+}
+
+const flyEnemy = new Flying (0, 0, 100, 100, "bob");
+flyEnemy.draw();
+console.log(flyEnemy.name);
